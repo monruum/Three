c8591f2b0509f1ab6d6d8f39998b1e64cecf948b
Alina <alina450245@gmail.com>
2022-02-11 09:25:10 +0700

    First commit

---
+++ Three/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Three
+//
+//  Created by Алина Нигма on 11.02.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
