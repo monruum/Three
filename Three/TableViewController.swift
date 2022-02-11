00d082633dfc21c0c6431c0a0ae346d5fbc994cf
Alina <alina450245@gmail.com>
2022-02-11 12:22:29 +0700

    add restaurants, restaurants logo and edit cell

--- Three/TableViewController.swift
+++ Three/TableViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var restArray = ["Академия Кофе","Organic Coffee","COFFEE COLLECTIVE","Surf coffee","Skuratov Coffee", "Starbucks"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,25 +24,21 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+      
+        return restArray.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
-
+        cell.textLabel?.text = restArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: restArray[indexPath.row])
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
